0584efb5faf0267d80e0a61a85629fc6398639ef
Bazith1 <mohammedbazith@outlook.com>
2024-12-25 12:19:15 +0530

    commit13

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hi juju helllo butty"
+    return "Hi juju , you are so beautiful , i love you , bujuku bujukku"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
