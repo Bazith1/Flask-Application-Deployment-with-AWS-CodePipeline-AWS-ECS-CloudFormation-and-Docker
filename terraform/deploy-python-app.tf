bd12b23b7bfa76c6fd62d998cb6678dd94bd5409
Bazith1 <mohammedbazith@outlook.com>
2025-01-17 11:48:21 +0530

    Commit1

--- terraform/deploy-python-app.tf
+++
@@ -1,9 +0,0 @@
-#resource "null_resource" "deploy_python_app" {
-#  depends_on = [
-#    module.eks,  # Ensure EKS cluster is created first
-#  ]
-#
-#  provisioner "local-exec" {
-#    command = "chmod +x ./deploy.sh && ./deploy.sh ${module.eks.cluster_name}"
-# }
-#}
